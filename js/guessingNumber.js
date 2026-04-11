21e412a5c98b947fd98a61375fd06b44c7d71d7e
satria pamungkas <satriap1783@gmail.com>
2026-04-11 16:07:33 +0800

    feat rock, paper, scissors and number guessing game

---
+++ js/guessingNumber.js
@@ -0,0 +1,42 @@
+const numberToGuess = Math.floor(Math.random() *100) + 1;
+const guessInput = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const resultDisplay = document.getElementById("resultDisplay");
+const scoreDisplay = document.getElementById("scoreDisplay");
+const attemptDisplay = document.getElementById("attemptDisplay");
+const attemptCount = document.getElementById("attemptCount");
+let score = 0;
+let attempt = 4;
+
+guessButton.addEventListener("click",function(){
+    const playerGuess = parseInt(guessInput.value);
+    playGuessGame(playerGuess);
+});
+
+function playGuessGame(playerGuess){
+    let result = "";
+
+    if(attempt > 0){
+        if(playerGuess === numberToGuess){
+        result = "CONGRATS! YOU GUESSED THE NUMBER!";
+        score++;
+        scoreDisplay.textContent = score;
+        }else if(playerGuess < numberToGuess){
+            result = "TOO LOW! TRY AGAIN.";
+            attempt--;
+            attemptCount.textContent = attempt;
+            guessInput.value = "";
+        }else if(playerGuess > numberToGuess){
+            result = "TOO HIGH! TRY AGAIN.";
+            attempt--;
+            attemptCount.textContent = attempt;
+            guessInput.value = "";
+        }
+    }else{
+        result = "GAME OVER! THE NUMBER WAS " + numberToGuess;
+        guessInput.disabled = true;
+        guessButton.disabled = true;
+    }
+    
+    resultDisplay.textContent = "RESULT: " + result;
+}
